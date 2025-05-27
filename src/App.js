be169427759a65852622ab0d002a7dd7201f5f97
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-05-27 09:12:39 +0200

    borrar

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RobotTaskApp from './RobotTaskApp'; // Asegúrate que este path sea correcto
-
-function App() {
-  return <RobotTaskApp />;
-}
-
-export default App;
